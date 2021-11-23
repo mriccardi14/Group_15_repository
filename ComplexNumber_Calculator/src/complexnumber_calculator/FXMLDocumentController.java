21494f594a42fc33e759678a5d5f59426396ec13
Mario <yourgitemail@example.com>
2021-11-23 12:45:50 +0100

    Aggiornamento GUI 2

--- ComplexNumber_Calculator/src/complexnumber_calculator/FXMLDocumentController.java
+++ ComplexNumber_Calculator/src/complexnumber_calculator/FXMLDocumentController.java
@@ -6,7 +6,14 @@ package complexnumber_calculator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -14,9 +21,116 @@ import javafx.fxml.Initializable;
  */
 public class FXMLDocumentController implements Initializable {
 
+    @FXML
+    private SplitPane rootPane;
+    @FXML
+    private AnchorPane paneTop;
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private TableView<?> stack_value;
+    @FXML
+    private AnchorPane paneDown;
+    @FXML
+    private Button sqrt_btn;
+    @FXML
+    private Button dub_btn;
+    @FXML
+    private Button over_btn;
+    @FXML
+    private Button inverse_btn;
+    @FXML
+    private Button plusVar_btn;
+    @FXML
+    private Button drop_btn;
+    @FXML
+    private Button clear_btn;
+    @FXML
+    private Button subVar_btn;
+    @FXML
+    private Button swap_btn;
+    @FXML
+    private Button add_btn;
+    @FXML
+    private Button storeVar_btn;
+    @FXML
+    private Button mul_btn;
+    @FXML
+    private Button sub_btn;
+    @FXML
+    private Button div_btn;
+    @FXML
+    private Button equal_btn;
+    @FXML
+    private Button retrieve_btn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    @FXML
+    private void sqrt_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void dup_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void over_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void inverse_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void plusVar_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void drop_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void clear_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void subVar_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void swap_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void add_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void storeVar_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void mul_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void sub_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void div_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void equal_function(ActionEvent event) {
+    }
+
+    @FXML
+    private void retrieve_function(ActionEvent event) {
     }
 
 }
