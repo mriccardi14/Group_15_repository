376657d0179328ac0932ade1a1e64922fde497f5
Mario <yourgitemail@example.com>
2021-11-23 12:12:12 +0100

    Creazione Applicazione Java FX con implementazione classe Complex Number

---
+++ ComplexNumber_Calculator/src/complexnumber_calculator/FXMLDocumentController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package complexnumber_calculator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
